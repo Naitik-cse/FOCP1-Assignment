14c8fc7f76e1198fe79658c5438944230330f1ff
Naitik <danginaitikk@gmail.com>
2025-11-02 19:27:02 +0530

    Add Armstrong number checker in Q1.c
    Implement Armstrong number check for a given integer.

---
+++ Q1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+
+    int num;
+    int sum=0,inte;
+    int j=0;
+    printf("enter the number : ");
+    scanf("%d",&num);
+
+    for(int i=num;i>0;i=i/10){
+        
+        j++;
+    }
+
+for(int i=num;i>0;i=i/10){
+    inte=i%10;
+
+    sum=sum+pow(inte,j);
+}printf("%d",sum);
+if(sum==num){
+    printf("its armstrong ");
+}else{
+    printf("its not armstrong ");
+}
+    return 0;
+}
